64991608c58948a94c0a0253c8777b388b4f22e3
Pedro Medina Márquez <medinamarquezp@gmail.com>
2023-03-14 00:46:24 +0100

    refactor: token definition updated

--- contracts/RFRToken.sol
+++ contracts/RFRToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract RFRToken is ERC20 {
-    constructor() ERC20("RFRT", "Roadmap Feature Request Token") {
-        _mint(msg.sender, 1000000);
+    constructor() ERC20("Roadmap Feature Request Token", "RFRT") {
+        _mint(msg.sender, 100000000 * 10 ** 18);
     }
 }
