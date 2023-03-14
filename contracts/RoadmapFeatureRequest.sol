f4148c48fd948914eb34853aa4ff63f6dd916d7f
Pedro Medina Márquez <medinamarquezp@gmail.com>
2023-03-15 00:52:45 +0100

    feat: data structures

--- contracts/RoadmapFeatureRequest.sol
+++ contracts/RoadmapFeatureRequest.sol
@@ -4,5 +4,30 @@ pragma solidity ^0.8.19;
 import "./Swap.sol";
 
 contract RoadmapFeatureRequest is Swap {
-    constructor(address _token) Swap(_token) {}
+    enum Status {
+        Pending,
+        Accepted,
+        InProgress,
+        Shipped,
+        Rejected
+    }
+
+    struct FeatureRequest {
+        uint256 id;
+        address owner;
+        string title;
+        string description;
+        Status status;
+        uint256 votes;
+        uint256 createdAt;
+        uint256 rejectedAt;
+    }
+
+    uint256 featureId;
+    mapping(uint256 => FeatureRequest) public features;
+    mapping(Status => FeatureRequest[]) public roadmap;
+
+    constructor(address _token) Swap(_token) {
+        featureId = block.timestamp;
+    }
 }
