9996a693cbab30522cb7d62cb0c9a8e310118c32
Pedro Medina Márquez <medinamarquezp@gmail.com>
2023-03-14 23:32:16 +0100

    refactor: use method in message sender

--- contracts/RFRToken.sol
+++ contracts/RFRToken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract RFRToken is ERC20 {
     constructor() ERC20("Roadmap Feature Request Token", "RFRT") {
-        _mint(msg.sender, 100000000 * 10 ** 18);
+        _mint(_msgSender(), 100000000 * 10 ** 18);
     }
 }
